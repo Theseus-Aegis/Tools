470b57bb8fb375684b36a88f1ba6246b5514bff4
jonpas <jonpas33@gmail.com>
2020-06-16 22:24:23 +0200

    Fix backup script duplicate archive file names and adapt to pep8

--- tools/server/backup.py
+++ tools/server/backup.py
@@ -9,24 +9,29 @@ import subprocess
 import ftplib
 import stat
 
-######## GLOBALS #########
+
 # PATHS
 TEMP = "C:\\Theseus\\Tools\\temp"
 ARMA_SERVERS_PATH = "C:\\Theseus\\Arma 3\\Servers"
 MYSQLDUMP_PATH = "C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin"
 STORAGE = "C:\\Theseus\\Backup"
 # CONTENTS
-ITEMS = ["C:\\Apache24", "C:\\php7", "C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Data", "C:\\ProgramData\\MySQL\\MySQL Server 5.7\\my.ini", "C:\\Theseus\\Arma 3\\Missions Archive", "C:\\Theseus\\Athena", "C:\\Theseus\\TeamSpeak 3 Server", "C:\\Theseus\\www\\drupal", "C:\\Theseus\\www\\resources\\TheseusServices", "C:\\Theseus\\www\\squadxml", "C:\\Theseus\\tac_notes.txt"]
-ARMA_SERVER_ITEMS = ["Apollo", "mpmissions", "apollo.properties", "jni.conf", "jni.dll", "jni_x64.dll", "params.cfg", "server.cfg"]
+ITEMS = ["C:\\Apache24", "C:\\php7", "C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Data",
+         "C:\\ProgramData\\MySQL\\MySQL Server 5.7\\my.ini", "C:\\Theseus\\Arma 3\\Missions Archive",
+         "C:\\Theseus\\Athena", "C:\\Theseus\\TeamSpeak 3 Server", "C:\\Theseus\\www\\drupal",
+         "C:\\Theseus\\www\\resources\\TheseusServices", "C:\\Theseus\\www\\squadxml", "C:\\Theseus\\tac_notes.txt"]
+ARMA_SERVER_ITEMS = ["Apollo", "mpmissions", "apollo.properties", "jni.conf", "jni.dll", "jni_x64.dll", "params.cfg",
+                     "server.cfg"]
 DATABASES = ["apollo", "apollo_test", "drupal"]
 # OTHER
 BACKUPS_TO_KEEP = 10
-##########################
+
 
 def log(text):
     print(text)
     with open("backup.log", "a") as logfile:
         logfile.write("[{}] {}\n".format(time.strftime("%Y%m%dT%H%M%S"), text))
+
 
 def remove_readonly(function, path, exc):
     os.chmod(path, stat.S_IWRITE)
@@ -40,9 +45,12 @@ def main():
     parser.add_argument("-dbp", "--databasepassword", default=False, type=str, required=False, help="database password")
     parser.add_argument("-ftps", "--ftpserver", default=False, type=str, required=False, help="FTP backup server URL")
     parser.add_argument("-ftpu", "--ftpuser", default=False, type=str, required=False, help="FTP backup server user")
-    parser.add_argument("-ftpp", "--ftppassword", default=False, type=str, required=False, help="FTP backup server password")
-    parser.add_argument("-si", "--specificitem", default=False, type=str, required=False, help="backup only specific item")
-    parser.add_argument("-sdb", "--specificdatabase", default=False, type=str, required=False, help="backup only specific database")
+    parser.add_argument("-ftpp", "--ftppassword", default=False, type=str, required=False,
+                        help="FTP backup server password")
+    parser.add_argument("-si", "--specificitem", default=False, type=str, required=False,
+                        help="backup only specific item")
+    parser.add_argument("-sdb", "--specificdatabase", default=False, type=str, required=False,
+                        help="backup only specific database")
     keep_parser = parser.add_mutually_exclusive_group(required=False)
     keep_parser.add_argument("-k", "--keep", dest="keep", action="store_true", help="keep backup locally")
     keep_parser.add_argument("-nk", "--no-keep", dest="keep", action="store_false", help="remove local backup")
@@ -57,7 +65,7 @@ def main():
     if args.specificdatabase:
         databases = [args.specificdatabase]
 
-
+    ###
     # Prepare temporary folders and date
     date = time.strftime("%Y%m%dT%H%M%S")
     log("Starting backup!")
@@ -72,7 +80,7 @@ def main():
     os.mkdir(TEMP)
     os.mkdir(packedFiles)
 
-
+    ###
     # Backup items
     log("Backing up items...")
 
@@ -84,54 +92,53 @@ def main():
             os.mkdir(tempFiles)
             if os.path.isdir(item):
                 tempFolder = item.rsplit("\\", 1)[1]
-                tempFolder = os.path.join(tempFiles,tempFolder)
-                shutil.copytree(item,tempFolder)
+                tempFolder = os.path.join(tempFiles, tempFolder)
+                shutil.copytree(item, tempFolder)
             else:
-                shutil.copy2(item,tempFiles)
+                shutil.copy2(item, tempFiles)
 
             # Zip it up and move it to another folder
             name = item[3:].replace(" ", "_").replace("\\", "_").replace("__", "_").lower()
             if "." in name:
-                name = name.split(".", 1)[0]
+                name = name.rsplit(".", 1)[0]
 
-            archive = shutil.make_archive("{}_{}".format(date,name), "zip", tempFiles)
-            shutil.move(archive,packedFiles)
+            archive = shutil.make_archive("{}_{}".format(date, name), "zip", tempFiles)
+            shutil.move(archive, packedFiles)
 
             # Remove temporary folder to allow copytree to create it again
             shutil.rmtree(tempFiles, onerror=remove_readonly)
-
 
     # Backup Arma 3 Server items
     log("Backing up Arma 3 Server items...")
 
     if os.path.exists(ARMA_SERVERS_PATH):
         for serverDir in os.listdir(ARMA_SERVERS_PATH):
-            serverPath = os.path.join(ARMA_SERVERS_PATH,serverDir)
+            serverPath = os.path.join(ARMA_SERVERS_PATH, serverDir)
             log("- {}".format(serverPath))
 
             os.mkdir(tempFiles)
             for item in ARMA_SERVER_ITEMS:
-                itemPath = os.path.join(serverPath,item)
+                itemPath = os.path.join(serverPath, item)
                 if os.path.exists(itemPath):
                     # Copy to temporary folder to prevent access issues
                     if os.path.isdir(itemPath):
                         tempFolder = itemPath.rsplit("\\", 1)[1]
-                        tempFolder = os.path.join(tempFiles,tempFolder)
-                        shutil.copytree(itemPath,tempFolder)
+                        tempFolder = os.path.join(tempFiles, tempFolder)
+                        shutil.copytree(itemPath, tempFolder)
                     else:
-                        shutil.copy2(itemPath,tempFiles)
+                        shutil.copy2(itemPath, tempFiles)
 
             # Zip it up and move it to another folder
             name = ARMA_SERVERS_PATH[3:].replace(" ", "_").replace("\\", "_")
-            name = "{}_{}".format(name,serverDir).lower()
+            name = "{}_{}".format(name, serverDir).lower()
 
-            archive = shutil.make_archive("{}_{}".format(date,name), "zip", tempFiles)
-            shutil.move(archive,packedFiles)
+            archive = shutil.make_archive("{}_{}".format(date, name), "zip", tempFiles)
+            shutil.move(archive, packedFiles)
 
             # Remove temporary folder to allow copytree to create it again
             shutil.rmtree(tempFiles, onerror=remove_readonly)
 
-
+    ###
     # Export databases
     if args.databaseuser and args.databasepassword:
         log("Backing up databases...")
@@ -141,42 +148,42 @@ def main():
             log("- {}".format(database))
 
             subprocess.call([
-                "{}\mysqldump".format(MYSQLDUMP_PATH),
+                "{}\\mysqldump".format(MYSQLDUMP_PATH),
                 "-u",
                 "{}".format(args.databaseuser),
                 "-p{}".format(args.databasepassword),
                 "{}".format(database),
                 ">",
-                "{}\{}.sql".format(tempFiles,database)
+                "{}\\{}.sql".format(tempFiles, database)
             ], shell=True)
 
         # Zip it up and move it to another folder
         archive = shutil.make_archive("{}_mysql_dump".format(date), "zip", tempFiles)
-        shutil.move(archive,packedFiles)
+        shutil.move(archive, packedFiles)
 
         # Remove temporary folder to allow copytree to create it again
         shutil.rmtree(tempFiles, onerror=remove_readonly)
     else:
         log("Skipping database backup (no user and password)")
 
-
+    ###
     # Upload to FTP server
     if args.ftpserver and args.ftpuser and args.ftppassword:
         log("Uploading to FTP server...")
 
         # Establish FTP connection
         ftp = ftplib.FTP(args.ftpserver)
-        ftp.login(args.ftpuser,args.ftppassword)
+        ftp.login(args.ftpuser, args.ftppassword)
 
         # Upload files
         folder = "{}_theseus_backup".format(date)
 
-        if not folder in ftp.nlst():
+        if folder not in ftp.nlst():
             ftp.mkd(folder)
 
         ftp.cwd(folder)
         for file in os.listdir(packedFiles):
-            filePath = os.path.join(packedFiles,file)
+            filePath = os.path.join(packedFiles, file)
             log("- {}".format(filePath))
             ftp.storbinary("STOR {}".format(file), open(filePath, "rb"), 1024)
 
@@ -184,7 +191,7 @@ def main():
         ftp.cwd("..")
         folders = [x for x in ftp.nlst() if "_theseus_backup" in x]
         folders.sort()
-        amountToRemove = len(folders) - BACKUPS_TO_KEEP - 4 # 4 = [".", "..", ".banner", ".ftpquota"]
+        amountToRemove = len(folders) - BACKUPS_TO_KEEP - 4  # 4 = [".", "..", ".banner", ".ftpquota"]
         if amountToRemove > 0:
             log("- Removing {} old backup(s)...".format(amountToRemove))
 
@@ -202,7 +209,7 @@ def main():
     else:
         log("Skipping upload to FTP server (no user and password)")
 
-
+    ###
     # Move to specified storage location or remove
     if args.keep:
         log("Moving to storage location...")
@@ -210,8 +217,8 @@ def main():
         if not os.path.exists(STORAGE):
             os.mkdir(STORAGE)
 
-        folder = "{}\\{}_theseus_backup".format(STORAGE,date)
-        shutil.move(packedFiles,folder)
+        folder = "{}\\{}_theseus_backup".format(STORAGE, date)
+        shutil.move(packedFiles, folder)
 
         # Remove oldest backup when there are more than BACKUPS_TO_KEEP
         folders = [x for x in os.listdir(STORAGE) if "_theseus_backup" in x]
@@ -222,7 +229,7 @@ def main():
 
             for folder in folders[:amountToRemove]:
                 log("--- {}".format(folder))
-                shutil.rmtree(os.path.join(STORAGE,folder), onerror=remove_readonly)
+                shutil.rmtree(os.path.join(STORAGE, folder), onerror=remove_readonly)
         else:
             log("- Skipping removal of old backups (not enough of them)")
     else:
@@ -234,7 +241,8 @@ def main():
 if __name__ == "__main__":
     try:
         status = main()
-    except:
+    except Exception as e:
+        log(e)
         status = 1
     finally:
         # Remove temporary folder
