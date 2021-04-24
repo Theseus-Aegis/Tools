a5d358645c979468f90dacb6c9075fda030d44ca
jonpas <jonpas33@gmail.com>
2021-04-24 21:30:43 +0200

    Add a note to Armory fast import database script

--- server/dbscripts/armory_fast_import.sql
+++ server/dbscripts/armory_fast_import.sql
@@ -1,4 +1,5 @@
 TRUNCATE item_list;
+# Place CSV export in the following location and make sure there is an empty line at the end of the file
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/chronos.csv'
     INTO TABLE item_list
     FIELDS
