05645065c95974321d017250d75b6aa4bf7895eb
Silderoy <83008550+Silderoy@users.noreply.github.com>
2021-06-15 22:13:14 +0300

    Update server/Assignments_filler/sheetFiller.py
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- server/Assignments_filler/sheetFiller.py
+++ server/Assignments_filler/sheetFiller.py
@@ -94,7 +94,7 @@ def checker(service):
     query = (f"SELECT `nid`,`title` FROM drupal.node where title like \"%{writtenName}%\" ORDER BY nid DESC")
     contractList = get_from_db(query)
     queryLength = len(contractList)
-    if (queryLength == 0):
+    if queryLength == 0:
         oldNID = 0
         oldContractName = "empty name"
     elif (queryLength > 1):
