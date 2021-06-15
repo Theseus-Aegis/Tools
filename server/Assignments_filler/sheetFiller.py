588a901811d00f2a02694ada56d05d911ad710b0
Silderoy <83008550+Silderoy@users.noreply.github.com>
2021-06-15 22:15:21 +0300

    Update server/Assignments_filler/sheetFiller.py
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- server/Assignments_filler/sheetFiller.py
+++ server/Assignments_filler/sheetFiller.py
@@ -134,7 +134,7 @@ def checker(service):
         elif queryLength > 1:
             for sublist in contractList:
                 exactName = sublist[1]
-                if (writtenName == exactName):
+                if writtenName == exactName:
                     exactIndex = contractList.index(sublist)
                     newNID = contractList[exactIndex][0]
                     newContractName = contractList[exactIndex][1]
