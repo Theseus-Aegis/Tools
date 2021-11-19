a81b717e72be9996b40d962913392cc517137144
jonpas <jonpas33@gmail.com>
2021-11-19 01:38:53 +0100

    Address my own review

--- server/getTraitList.py
+++ server/getTraitList.py
@@ -1,27 +1,23 @@
-import glob, datetime
+import datetime
+import glob
 
-rptDir = "C:/Theseus/Arma 3/Servers/Contracts/profiles/"
-keyword = "Player Traits: "
-outputFile = "C:/Theseus/Tools/loggedTraits.log" # Requires full path
+RPT_DIR = "C:/Theseus/Arma 3/Servers/Contracts/profiles/"
+KEYWORD = "Player Traits: "
 
 # Get latest .rpt
-rptFiles = glob.glob(f"{rptDir}*.rpt")
+rptFiles = glob.glob(f"{RPT_DIR}*.rpt")
 rptFiles.sort(reverse=True)
 
 # Get list of keyword matches
 results = []
 with open(rptFiles[0], "r") as rpt_obj:
-    for l in rpt_obj:
-        if keyword in l:
-            results.append(l.rstrip())
+    for line in rpt_obj:
+        if KEYWORD in line:
+            results.append(line.rstrip())
 
-# Exit if no results
-if results == []:
-    exit()
-
-# Write to output file with timestamp
-output = open(outputFile, "a")
-for result in results:
-    result = result.split(keyword, 1)[1]
-    output.write(f"{datetime.date.today()} {result}" + "\n")
-output.close()
+if results:
+    # Write to output file with timestamp
+    with open("loggedTraits.log", "a") as logfile:
+        for result in results:
+            result = result.split(KEYWORD, 1)[1]
+            logfile.write(f"{datetime.date.today()} {result}\n")
