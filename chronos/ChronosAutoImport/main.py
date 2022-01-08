20717105c3154867c1d31976b965cf69739ac259
Flyzoor <38717032+Flyzoor@users.noreply.github.com>
2022-01-08 18:08:44 +0100

    Update main.py

--- chronos/ChronosAutoImport/main.py
+++ chronos/ChronosAutoImport/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import requests
 
 from getpass import getpass
@@ -7,17 +8,19 @@ from mysql.connector import connect, Error
 
 CHRONOS_LOCATION = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/"
 CHRONOS_NAME = "chronos.csv"
-SCRIPT_PATH = "C:\\Theseus\\Tools\\dbscripts\\armory_fast_import_min.sql"
-DB_NAME = "apollo_test"
+SCRIPT_PATH = "C:\\Theseus\\Tools\\dbscripts\\armory_fast_import.sql"
 
-#GOOGLE_DOCS_URL = "https://docs.google.com/spreadsheets/d/1ZGwujcqU2tdi1Y7l-sqeiB78O1mwAnGyAqWgeAVvQAk/gviz/tq?tqx=out:csv&sheet=item_list" # Theseus sheet
-GOOGLE_DOCS_URL = "https://docs.google.com/spreadsheets/d/1DJUc0hzYLLtpapYyJCLmwlzRp-SuPspFccf5WMJ5vPA/gviz/tq?tqx=out:csv&sheet=item_list"  # Ian's testing sheet
+DB_NAME = "apollo"
+GOOGLE_DOCS_URL = "https://docs.google.com/spreadsheets/d/1ZGwujcqU2tdi1Y7l-sqeiB78O1mwAnGyAqWgeAVvQAk/gviz/tq?tqx=out:csv&sheet=item_list" # Theseus sheet
+
+#DB_NAME = "apollo_test"
+#GOOGLE_DOCS_URL = "https://docs.google.com/spreadsheets/d/1DJUc0hzYLLtpapYyJCLmwlzRp-SuPspFccf5WMJ5vPA/gviz/tq?tqx=out:csv&sheet=item_list"  # Ian's testing sheet
 
 if __name__ == "__main__":
 
-    print("Downloading from Google Sheets...")
+    print("Downloading from google docs...")
     res = requests.get(GOOGLE_DOCS_URL)
-    assert res.status_code == 200, "Failed to download item list"
+    assert res.status_code == 200, 'Wrong status code'
 
     print("Saving file...")
     with open(f"raw_{CHRONOS_NAME}", "wb") as fp:
@@ -30,11 +33,11 @@ if __name__ == "__main__":
     print(f"\tGOT {linesDown} ITEMS")
     lines.append("\r\n")
 
-    # Check if file exists (it has to because windows and it's permissions....)
+    # Check if file exists (it has to do with windows and it's permissions....)
     if not os.path.isfile(f"{CHRONOS_LOCATION}{CHRONOS_NAME}"):
         print(f"ERROR: file {CHRONOS_LOCATION}{CHRONOS_NAME} could not be found, please create it.")
         input("Press any key to continiue...")
-        exit(1)
+        sys.exit()
 
     #Writing to Uploads/chronos.csv file...
     with open(f"{CHRONOS_LOCATION}{CHRONOS_NAME}", "w") as fp:
@@ -51,10 +54,17 @@ if __name__ == "__main__":
         ) as connection:
             print(connection)
             cursor = connection.cursor()
-            with open(SCRIPT_PATH) as fp:
+            
+            with open(SCRIPT_PATH, "r") as fp:
                 sqlLines = fp.readlines()
+
+            sqlLines = [line.strip() for line in sqlLines if not line.strip().startswith("#")]
+            sqlLines = " ".join(sqlLines)
+            sqlLines = sqlLines.split("; ")
+
             for line in sqlLines:
                 cursor.execute(line)
+
             linesAffected = cursor.rowcount
             print(f"\tAFFECTED {linesAffected} LINES")
 
@@ -62,10 +72,10 @@ if __name__ == "__main__":
                 print("MISMATCH IN NUMBER OF DOWNLOADED AND NUMBER OF AFFECTED LINES !")
                 print("\tCONFIRM COMMIT ? Y/[N]")
 
-                if input().lower() != "y":
+                if input() not in ["y", "Y"]:
                     print("Exiting...")
                     input("Press any key to continiue...")
-                    exit(1)
+                    sys.exit()
 
             print(f"Saving to DB {DB_NAME}...")
             connection.commit()
