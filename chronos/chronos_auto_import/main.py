35c76a1d6721a5c27c1e8f8290ef0157199851d9
Flyzoor <38717032+Flyzoor@users.noreply.github.com>
2022-01-08 19:21:54 +0100

    Add Chronos Auto Import (#41)
    * Added ChronosAutoImport script
    
    * Apply suggestions from code review
    
    Co-authored-by: jonpas <jonpas33@gmail.com>
    
    * Update chronos/ChronosAutoImport/runImport.bat
    
    Co-authored-by: jonpas <jonpas33@gmail.com>
    
    * Update chronos/ChronosAutoImport/main.py
    
    Co-authored-by: TyroneMF <TyroneMF@hotmail.com>
    
    * Update main.py
    
    * snake_case folder
    
    * Update chronos/chronos_auto_import/readme.md
    
    * Update requirements.txt
    
    * Update runImport.bat
    
    Co-authored-by: jonpas <jonpas33@gmail.com>
    Co-authored-by: TyroneMF <TyroneMF@hotmail.com>

---
+++ chronos/chronos_auto_import/main.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import requests
+
+from getpass import getpass
+from mysql.connector import connect, Error
+
+
+CHRONOS_LOCATION = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/"
+CHRONOS_NAME = "chronos.csv"
+SCRIPT_PATH = "C:\\Theseus\\Tools\\dbscripts\\armory_fast_import.sql"
+
+DB_NAME = "apollo"
+GOOGLE_DOCS_URL = "https://docs.google.com/spreadsheets/d/1ZGwujcqU2tdi1Y7l-sqeiB78O1mwAnGyAqWgeAVvQAk/gviz/tq?tqx=out:csv&sheet=item_list" # Theseus sheet
+
+#DB_NAME = "apollo_test"
+#GOOGLE_DOCS_URL = "https://docs.google.com/spreadsheets/d/1DJUc0hzYLLtpapYyJCLmwlzRp-SuPspFccf5WMJ5vPA/gviz/tq?tqx=out:csv&sheet=item_list"  # Ian's testing sheet
+
+if __name__ == "__main__":
+
+    print("Downloading from google docs...")
+    res = requests.get(GOOGLE_DOCS_URL)
+    assert res.status_code == 200, 'Wrong status code'
+
+    print("Saving file...")
+    with open(f"raw_{CHRONOS_NAME}", "wb") as fp:
+        fp.write(res.content)
+
+    with open(f"raw_{CHRONOS_NAME}") as fp:
+        lines = fp.readlines()
+
+    linesDown = len(lines)-1
+    print(f"\tGOT {linesDown} ITEMS")
+    lines.append("\r\n")
+
+    # Check if file exists (it has to do with windows and it's permissions....)
+    if not os.path.isfile(f"{CHRONOS_LOCATION}{CHRONOS_NAME}"):
+        print(f"ERROR: file {CHRONOS_LOCATION}{CHRONOS_NAME} could not be found, please create it.")
+        input("Press any key to continiue...")
+        sys.exit()
+
+    #Writing to Uploads/chronos.csv file...
+    with open(f"{CHRONOS_LOCATION}{CHRONOS_NAME}", "w") as fp:
+        fp.writelines(lines)
+
+    print("Connecting to DB...")
+    try:
+        with connect(
+            host="localhost",
+            database=f"{DB_NAME}",
+            allow_local_infile=True,
+            user=input("\tEnter username: "),
+            password=getpass("\tEnter password: "),
+        ) as connection:
+            print(connection)
+            cursor = connection.cursor()
+            
+            with open(SCRIPT_PATH, "r") as fp:
+                sqlLines = fp.readlines()
+
+            sqlLines = [line.strip() for line in sqlLines if not line.strip().startswith("#")]
+            sqlLines = " ".join(sqlLines)
+            sqlLines = sqlLines.split("; ")
+
+            for line in sqlLines:
+                cursor.execute(line)
+
+            linesAffected = cursor.rowcount
+            print(f"\tAFFECTED {linesAffected} LINES")
+
+            if linesAffected != linesDown:
+                print("MISMATCH IN NUMBER OF DOWNLOADED AND NUMBER OF AFFECTED LINES !")
+                print("\tCONFIRM COMMIT ? Y/[N]")
+
+                if input() not in ["y", "Y"]:
+                    print("Exiting...")
+                    input("Press any key to continiue...")
+                    sys.exit()
+
+            print(f"Saving to DB {DB_NAME}...")
+            connection.commit()
+
+    except Error as e:
+        print(e)
+
+    input("DONE, press any key to continue...")
---
+++ chronos/chronos_auto_import/runImport.bat
@@ -0,0 +1,3 @@
+cd C:\Theseus\Tools\chronos_auto_import
+venv\Scripts\activate.bat && python main.py && deactivate
+pause
