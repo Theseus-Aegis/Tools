74c7038399a81a9b9510116f23939cdbb676dbbb
jonpas <jonpas33@gmail.com>
2022-09-29 04:50:33 +0200

    Add loadout refunding to Armory Refunds script

--- chronos/armory_refunds.py
+++ chronos/armory_refunds.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import ast
 import sys
 import os
 import csv
 from collections import defaultdict
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields, astuple
 
 from mysql.connector import connect
 
@@ -44,10 +45,210 @@ class Refund:
     items: list = field(default_factory=list)
     note: list = field(default_factory=list)
     balance: int = 0  # new bank account balance
-    inventory: str = ""  # new inventory
+    loadout: str = ""  # new loadout
+
+
+@dataclass
+class LoadoutItem:
+    classname: str = ""
+    quantity: int = 0
+    bullet_count: int = None
+    is_backpack: bool = None
+
+    def __init__(self, *args, muzzle=False):
+        if len(args) > 0:
+            self.classname = args[0]
+
+            if type(args[1]) is bool:
+                self.is_backpack = args[1]
+                self.quantity = 1
+            elif len(args) > 1:
+                if muzzle:
+                    self.quantity = None
+                    self.bullet_count = args[1]
+                else:
+                    self.quantity = args[1]
+
+                if len(args) > 2:
+                    self.bullet_count = args[2]
+
+    def find_and_remove(self, classname):
+        if classname == self.classname:
+            quantity = self.quantity if self.quantity else 1
+            self.classname = ""
+            self.quantity = 0
+            self.bullet_count = None
+            return quantity, True
+
+        return 0, False
+
+
+@dataclass
+class LoadoutWeapon:
+    classname: str = ""
+    suppressor: str = ""
+    pointer: str = ""
+    optics: str = ""
+    muzzle1: list = field(default_factory=list)
+    muzzle2: list = field(default_factory=list)
+    bipod: str = ""
+
+    def __post_init__(self):
+        self.muzzle1 = LoadoutItem(*self.muzzle1, muzzle=True)
+        self.muzzle2 = LoadoutItem(*self.muzzle2, muzzle=True)
+
+    def find_and_remove(self, classname):
+        if classname == self.classname:
+            self.classname += "<FOUND>"
+            return 1, False
+
+        for muzzle in [self.muzzle1, self.muzzle2]:
+            found = muzzle.find_and_remove(classname)
+            if found[0] > 0:
+                return found
+
+        return 0, False
+
+
+@dataclass
+class LoadoutContainer:
+    classname: str = ""
+    contents: list = field(default_factory=list)
+
+    def __post_init__(self):
+        loadout_contents = []
+        for content in self.contents:
+            if content:
+                if type(content[0]) is str:
+                    loadout_contents.append(LoadoutItem(*content))
+                else:
+                    for _ in range(content[1]):
+                        loadout_contents.append(LoadoutWeapon(*content[0]))
+
+        self.contents = loadout_contents
+
+    def find_and_remove(self, classname):
+        if classname == self.classname:
+            self.classname += "<FOUND>"
+            return 1, False
+
+        for content in self.contents:
+            found = content.find_and_remove(classname)
+            if found[0] > 0:
+                return found
+
+        return 0, False
+
+
+@dataclass
+class LoadoutAssignedItem:
+    item_map: str = ""
+    item_gps: str = ""
+    item_radio: str = ""
+    item_compass: str = ""
+    item_watch: str = ""
+    item_nvg: str = ""
+
+    def find_and_remove(self, classname):
+        for f in fields(self):
+            if classname == getattr(self, f.name):
+                setattr(self, f.name, "")
+                return 1, True
+        return 0, False
+
+
+@dataclass
+class Loadout:
+    primary: LoadoutWeapon
+    secondary: LoadoutWeapon
+    handgun: LoadoutWeapon
+    uniform: LoadoutContainer
+    vest: LoadoutContainer
+    backpack: LoadoutContainer
+    headgear: str
+    facewear: str
+    binoculars: LoadoutWeapon
+    assigned: LoadoutAssignedItem
+
+    def __init__(self, *args):
+        self.primary = LoadoutWeapon(*args[0])
+        self.secondary = LoadoutWeapon(*args[1])
+        self.handgun = LoadoutWeapon(*args[2])
+        self.uniform = LoadoutContainer(*args[3])
+        self.vest = LoadoutContainer(*args[4])
+        self.backpack = LoadoutContainer(*args[5])
+        self.headgear = args[6]
+        self.facewear = args[7]
+        self.binoculars = LoadoutWeapon(*args[8])
+        self.assigned = LoadoutAssignedItem(*args[9])
+
+    # return: (quantity, removed)
+    def find_and_remove(self, classname):
+        for f in fields(self):
+            fref = getattr(self, f.name)
+            ffind_and_remove = getattr(fref, "find_and_remove", None)
+            if f.type == str:
+                if classname == fref:
+                    setattr(self, f.name, "")
+                    return 1, True
+            else:
+                found = ffind_and_remove(classname)
+                if found[0] > 0:
+                    return found
+
+        return 0, False
+
+
+def listit(t):
+    return list(map(listit, t)) if isinstance(t, (list, tuple)) else t
+
+
+def loadout_from_arma(loadout_str):
+    if not loadout_str:
+        loadout_str = '[[],[],[],[],[],[],"","",[],["","","","","",""]]'
+    loadout = loadout_str.replace("true", "True").replace("false", "False")
+    loadout = ast.literal_eval(loadout)
+    loadout = Loadout(*loadout)
+    return loadout
+
+
+def loadout_to_arma(loadout):
+    loadout_list = listit(astuple(loadout))
+    loadout_str = str(loadout_list).replace(" ", "").replace("'", "\"").replace(",None", "").replace("<FOUND>", "")
+    loadout_str = loadout_str.replace("[\"\",0]", "[]").replace("[\"\",[]]", "[]").replace("[[]]", "[]")  # empty items and containers
+    loadout_str = loadout_str.replace("[\"\",\"\",\"\",\"\",[],[],\"\"]", "[]")  # empty weapons
+    return loadout_str
+
+
+def test():
+    loadout_str = '[[],[],[],[],[],["B_Bergen_dgtl_F",[["B_AssaultPack_blk",true],["RegItem",5],["Mag",5,30]]],"","",[],["ItemMap","","ItemRadioAcreFlagged","ItemCompass","ItemWatch",""]]'
+    # loadout_str = '[["arifle_ARX_blk_F","","","optic_Hamr",["30Rnd_65x39_caseless_green_mag_Tracer",20],["10Rnd_50BW_Mag_F",6],""],[],[],["tacs_Uniform_Plaid_JP_LP_BP_BB",[["ACE_fieldDressing",6],["tac_medical_painkillers",2],["ACE_tourniquet",2],["ACRE_PRC152",1]]],["CUP_Vest_RUS_6B45_Sh117",[["ACRE_PRC152",1],["ACE_bodyBag",1],["ACE_bloodIV",2],["ACE_bloodIV_250",3],["30Rnd_65x39_caseless_green",1,1],["30Rnd_65x39_caseless_green_mag_Tracer",1,4],["30Rnd_65x39_caseless_green",1,2],["30Rnd_65x39_caseless_green",1,9],["30Rnd_65x39_caseless_green",1,12],["30Rnd_65x39_caseless_green_mag_Tracer",1,10],["30Rnd_65x39_caseless_green_mag_Tracer",2,8]]],["B_Messenger_Black_F",[["ACE_EntrenchingTool",1],["ACE_bloodIV_500",1],["ACE_surgicalKit",1],["ACE_splint",1],["ACE_elasticBandage",5],["10Rnd_50BW_Mag_F",1,5],["30Rnd_65x39_caseless_green_mag_Tracer",1,9],["30Rnd_65x39_caseless_green_mag_Tracer",1,5],["30Rnd_65x39_caseless_green_mag_Tracer",1,10]]],"CUP_H_RUS_6B47_v2_Summer","",[],["ItemMap","","","ItemCompass","ItemWatch",""]]'
+    # loadout_str = '[["arifle_ARX_blk_F","","","optic_Hamr",[],["10Rnd_50BW_Mag_F",6],""],[],[],["tacs_Uniform_Plaid_JP_LP_BP_BB",[["ACE_fieldDressing",6],["tac_medical_painkillers",2],["ACE_tourniquet",2],["ACRE_PRC152",1]]],["CUP_Vest_RUS_6B45_Sh117",[["ACRE_PRC152",1],["ACE_bodyBag",1],["ACE_bloodIV",2],["ACE_bloodIV_250",3],["30Rnd_65x39_caseless_green",1,1],["30Rnd_65x39_caseless_green_mag_Tracer",1,4],["30Rnd_65x39_caseless_green",1,2],["30Rnd_65x39_caseless_green",1,9],["30Rnd_65x39_caseless_green",1,12],["30Rnd_65x39_caseless_green_mag_Tracer",1,10],["30Rnd_65x39_caseless_green_mag_Tracer",2,8]]],["B_Messenger_Black_F",[["ACE_EntrenchingTool",1],["ACE_bloodIV_500",1],["ACE_surgicalKit",1],["ACE_splint",1],["ACE_elasticBandage",5],["10Rnd_50BW_Mag_F",1,5],["30Rnd_65x39_caseless_green_mag_Tracer",1,9],["30Rnd_65x39_caseless_green_mag_Tracer",1,5],["30Rnd_65x39_caseless_green_mag_Tracer",1,10]]],"CUP_H_RUS_6B47_v2_Summer","",[],["ItemMap","","","ItemCompass","ItemWatch",""]]'
+    # loadout_str = '[[],[],[],[],[],["B_Bergen_dgtl_F",[[["CUP_sgun_AA12","","","",["CUP_20Rnd_B_AA12_HE",20],[],""],1],["B_AssaultPack_blk",true]]],"","",[],["ItemMap","","ItemRadioAcreFlagged","ItemCompass","ItemWatch",""]]'
+
+    loadout = loadout_from_arma(loadout_str)
+    # print(loadout)
+    # print()
+    # print(loadout.backpack)
+    # print()
+
+    search = "30Rnd_65x39_caseless_green_mag_Tracer"
+    # search = "arifle_ARX_blk_F"
+    # search = "ACE_elasticBandage"
+    # search = "CUP_20Rnd_B_AA12_HE"
+    while (found := loadout.find_and_remove(search))[0] > 0:
+        print("found", found)
+    print("\nloadout", loadout)
+
+    parsed = loadout_to_arma(loadout)
+    print("\nparsed", parsed)
+
+    return 0
 
 
 def main():
+    # test()
+
     # Parse arguments
     parser = argparse.ArgumentParser(description="Armory Refunds")
     parser.add_argument("old", type=str, help="old armory sheet (CSV)")
@@ -60,11 +261,11 @@ def main():
     if not os.path.exists(args.old):
         parser.error(f"error! file '{args.old}' does not exist")
 
-    players = dict()  # playerid => uid
+    players = dict()  # playerid => uid and uid => playerid
     old_item_list = dict()  # classname => Item
     item_list = dict()  # classname => Item
     refunds = defaultdict(Refund)  # uid => Refund
-    inventory_unremovable = defaultdict(list)  # classname => [playerids]
+    loadout_unremovable = defaultdict(list)  # classname => [playerids]
 
     # assemble old items and save their data (prices)
     with open(args.old) as f:
@@ -81,6 +282,7 @@ def main():
         cursor.execute(f"SELECT entity_id, field_player_id_value FROM {TABLE_IDS}")
         for (uid, playerid) in cursor:
             players[playerid] = uid
+            players[uid] = playerid
 
     # find removed items and assemble all data
     with connect(host=args.databasehost, database=DB_NAME, user=args.databaseuser, password=args.databasepassword) as conn:
@@ -104,38 +306,36 @@ def main():
                 else:
                     refunds[uid].items.append(Item(classname, "", 0))
                     print(f"warning: '{classname}' owned by {uid} not found in old item list - it will be removed without refund")
-                    input("    confirm?")
+                    #input("  confirm?")
 
-        # inventory
+        # loadout
         cursor.execute(f"SELECT playerID, loadout FROM {TABLE_EQUIPPED}")
-        for (playerid, loadout) in cursor:
+        for (playerid, loadout_orig) in cursor:
             uid = players[playerid]
+            refunds[uid].uid = uid
+
+            loadout_orig = loadout_orig.lower()
+            loadout = loadout_from_arma(loadout_orig)
+
             for old_item in old_item_list.values():
-                loadout = loadout.lower().replace(" ", "")  # remove any spacing
-                if old_item.classname not in item_list.keys() and old_item.classname in loadout:
-                    item_index_offset = -1
-                    item_index = - 1  # index of classname start (without quote)
-                    quantity = 0
-                    while (item_index := loadout.find(old_item.classname, item_index + 1)) != -1:
-                        quantity_index = loadout.find(",", item_index) + 1  # index of quantity start
-                        if quantity_index - item_index - 2 > len(old_item.classname):
+                if old_item.classname not in item_list.keys():
+                    while (fnr := loadout.find_and_remove(old_item.classname))[0] > 0:
+                        quantity, removed = fnr
+                        if not removed:
                             print(f"warning: unable to remove '{old_item.classname}' from {uid} ({playerid}) - item will be refunded, but not removed!")
-                            input("    confirm?")
-                            inventory_unremovable[old_item.classname].append(playerid)
-                            item_index_offset = item_index
-                        else:
-                            item_index_end = quantity_index - 2  # index of classname end (without quotes)
-                            quantity_index_end = loadout.find("]", quantity_index)  # index of quantity end
+                            found_index = loadout_orig.find(old_item.classname)
+                            print(f"    {loadout_orig[max(0, found_index - 15):found_index + len(old_item.classname) + 100]}")
+                            input("  confirm?")
+                            loadout_unremovable[old_item.classname].append(playerid)
 
-                            quantity += int(loadout[quantity_index:quantity_index_end])
-                            loadout = loadout[:item_index] + loadout[item_index_end:]
-                            item_index = item_index_offset
+                        refunds[uid].total += quantity * old_item.price
+                        refunds[uid].note.append(f"{quantity}x {old_item.prettyname}")
 
-                    refunds[uid].uid = uid
-                    refunds[uid].total += quantity * old_item.price
-                    refunds[uid].items.append(old_item)
-                    refunds[uid].note.append(f"{quantity}x {old_item.prettyname}")
-                    refunds[uid].inventory = loadout
+            loadout = loadout_to_arma(loadout)
+            if loadout != loadout_orig:
+                print(f"\n{loadout_orig}\nto\n{loadout}\n")
+                refunds[uid].loadout = loadout
+
 
         # process new balances
         cursor.execute(f"SELECT id, balance FROM {TABLE_BANKACCOUNTS}")
@@ -144,70 +344,64 @@ def main():
                 refunds[uid].balance = balance + refunds[uid].total
 
     # notes and total
-    print()
+    print("\n--- TOTALS ---\n")
     totalsum = 0
     for refund in refunds.values():
         if refund.total > 0:
             print(f"{refund.uid} ({refund.total}$): Refund for removal of {', '.join(refund.note)}.")
             totalsum += refund.total
 
-    print(f"\nTotal refunds: {totalsum}$\n")
+    print(f"\nTotal refunds: {totalsum}$")
 
-    # remove locker items
+    print("\n--- QUERIES ---\n")
     with connect(host=args.databasehost, database=DB_NAME, user=args.databaseuser, password=args.databasepassword) as conn:
         cursor = conn.cursor()
+        conn.autocommit = True
 
-        query = f"FROM {TABLE_LOCKER} WHERE"
+        # remove locker items
+        query = ""
         for refund in refunds.values():
             for item in refund.items:
                 query += f" (uid={refund.uid} AND className='{item.classname}') OR"
         query = query[:-3]
 
-        if args.execute:
-            cursor.execute(f"DELETE {query}")
-            conn.commit()
-        else:
-            print(query)
+        print(f"DELETE FROM {TABLE_LOCKER} WHERE {query};\n")
+        if args.execute and query:  # check query! empty query will delete whole table!
+            cursor.execute(f"DELETE FROM {TABLE_LOCKER} WHERE {query};")
 
-    # remove inventory items
-    print()
-    with connect(host=args.databasehost, database=DB_NAME, user=args.databaseuser, password=args.databasepassword) as conn:
-        cursor = conn.cursor()
-
-        query = ""
+        # remove loadout items
         for refund in refunds.values():
-            if refund.inventory:
-                query += f"UPDATE {TABLE_EQUIPPED} SET loadout = {refund.inventory} WHERE uid={refund.uid};\n"
+            if refund.loadout:
+                query = f"UPDATE {TABLE_EQUIPPED} SET loadout = '{refund.loadout}' WHERE playerid={players[refund.uid]};"
 
-        if args.execute:
-            cursor.execute(query)
-            conn.commit()
-        else:
-            print(query)
+                print(query)
+                if args.execute:
+                    cursor.execute(query)
 
-    # refund
-    with connect(host=args.databasehost, database=DB_NAME, user=args.databaseuser, password=args.databasepassword) as conn:
-        cursor = conn.cursor()
-
-        query = ""
+        # refund
+        print()
         for refund in refunds.values():
             if refund.total > 0:
-                query += f"UPDATE {TABLE_BANKACCOUNTS} SET balance = {refund.balance} WHERE uid={refund.uid};\n"
-                query += f"INSERT INTO {TABLE_TRANSACTIONS} (transactionInitiator, transactionReceiver, type, description, cost, balance, category)\n"
-                refund_note = ", ".join(refund.note)
-                query += f"    VALUES (0, {refund.uid}, 'Money Transfer', '{REFUND_PREFIX} {refund_note}', {refund.total}, {refund.balance}, 'Transfer');\n"
+                query = f"UPDATE {TABLE_BANKACCOUNTS} SET balance = {refund.balance} WHERE id={refund.uid};"
 
-        if args.execute:
-            cursor.execute(query)
-            conn.commit()
-        else:
-            print(query)
+                print(query)
+                if args.execute:
+                    cursor.execute(query)
+
+                refund_note = ", ".join(refund.note)
+                query = f"INSERT INTO {TABLE_TRANSACTIONS} (transactionInitiator, transactionReceiver, type, description, cost, balance, category)\n"
+                query += f"  VALUES (0, {refund.uid}, 'Money Transfer', '{REFUND_PREFIX} {refund_note}', {refund.total}, {refund.balance}, 'Transfer');"
+
+                print(query)
+                if args.execute:
+                    cursor.execute(query)
 
     # final notes
-    if len(inventory_unremovable) > 0:
-        print("Remaining manual work!")
+    if len(loadout_unremovable) > 0:
+        print("\n--- NOTES ---\n")
+        print("remaining manual work!")
 
-        for classname, playerids in inventory_unremovable.items():
+        for classname, playerids in loadout_unremovable.items():
             print(f"'{classname}' must be manually removed from inventories:")
             for playerid in playerids:
                 print(f"    {playerid}")
