0c3fe0b8b1e2b50fb41e66efecd3cad679285093
jonpas <jonpas33@gmail.com>
2023-06-14 23:34:23 +0200

    Simplify userconfig

--- arma3/userconfig/tac/auth.hpp
+++ arma3/userconfig/tac/auth.hpp
@@ -1,1 +1,12 @@
 #define SERVER_COMMAND_PASSWORD "1235235changeThis64311"
+
+//#define DEBUG_ADMINS // hardcoded in template's admin/script_component.hpp due to local resolution
+#define ADMINS \
+    "76561198048995566", /* Jonpas */    \
+    "76561198085500182", /* Mike */      \
+    "76561198040879834", /* JoramD */    \
+    "76561198024182729", /* Laird */     \
+    "76561198062152747", /* Ethan */     \
+    "76561198021011658", /* Zakharov */  \
+    "76561198324654204", /* gilleedoo */ \
+    "76561197985332763"  /* rg */
